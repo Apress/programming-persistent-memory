d7431b89150c3b40f1b93b0e5858671ef4b59de3
Ken Gibson <kenneth.j.gibson@intel.com>
2019-05-09 07:24:05 -0600

    Adding pmem map file and persist with is_pmem example

---
+++ chapter3/pmem_map_file.c
@@ -0,0 +1,45 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#ifndef _WIN32
+#include <unistd.h>
+#else
+#include <io.h>
+#endif
+#include <string.h>
+#include <libpmem.h>
+
+/* using 4k of pmem for this example */
+#define PMEM_LEN 4096
+
+#define PATH "/pmem-fs/myfile"
+
+int
+main(int argc, char *argv[])
+{
+	char *pmemaddr;
+	size_t mapped_len;
+	int is_pmem;
+
+	/* create a pmem file and memory map it */
+	if ((pmemaddr = pmem_map_file(PATH, PMEM_LEN, PMEM_FILE_CREATE,
+				0666, &mapped_len, &is_pmem)) == NULL) {
+		perror("pmem_map_file");
+		exit(1);
+	}
+
+	/* store a string to the persistent memory */
+	strcpy(pmemaddr, "hello, persistent memory");
+
+	/* flush above strcpy to persistence */
+	if (is_pmem)
+		pmem_persist(pmemaddr, mapped_len);
+	else
+		pmem_msync(pmemaddr, mapped_len);
+
+	/* Delete the mappings. The region is also */
+	pmem_unmap(pmemaddr, mapped_len);
+}
