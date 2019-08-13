86411cbd3f6931ea61c79c87352b4d7edac42a0c
Andy Rudoff <andy.rudoff@intel.com>
2019-08-12 19:04:21 -0600

    style & formatting tweaks

--- chapter1/pmemkv.cpp
+++ chapter1/pmemkv.cpp
@@ -31,7 +31,7 @@
  */
 
 /*
- * pmemkv.cpp -- demo of persistent memory key-value store
+ * pmemkv.cpp -- demonstrate a high-level key-value API for pmem
  */
 
 #include <iostream>
@@ -44,66 +44,65 @@ using std::cout;
 using std::endl;
 using std::string;
 
+/* for this example, create a 1 Gig file called "/daxfs/kvfile" */
 auto PATH = "/daxfs/kvfile";
-
-const uint64_t SIZE = 1024 * 1024 * 1024;  // 1 Gig
+const uint64_t SIZE = 1024 * 1024 * 1024;
 
 /*
  * kvprint -- print a single key-value pair
  */
 int kvprint(string_view k, string_view v) {
-    cout << "key: " << k.data() << ", value: " << v.data() << endl;
-    return 0;
+	cout << "key: " << k.data() << ", value: " << v.data() << endl;
+	return 0;
 }
 
 int main() {
+	// start by creating the db object
+	db *kv = new db();
+	assert(kv != nullptr);
 
-    // create config and set all required options
-    config cfg;
+	// create the config information for libpmemkv's open method
+	config cfg;
 
-    if (cfg.put_string("path", PATH) != status::OK) {
-        cerr << pmemkv_errormsg() << endl;
-        return 1;
-    }
+	if (cfg.put_string("path", PATH) != status::OK) {
+		cerr << pmemkv_errormsg() << endl;
+		exit(1);
+	}
+	if (cfg.put_uint64("force_create", 1) != status::OK) {
+		cerr << pmemkv_errormsg() << endl;
+		exit(1);
+	}
+	if (cfg.put_uint64("size", SIZE) != status::OK) {
+		cerr << pmemkv_errormsg() << endl;
+		exit(1);
+	}
 
-    if (cfg.put_uint64("force_create", 1) != status::OK) {
-        cerr << pmemkv_errormsg() << endl;
-        return 1;
-    }
 
-    if (cfg.put_uint64("size", SIZE) != status::OK) {
-        cerr << pmemkv_errormsg() << endl;
-        return 1;
-    }
+	// open the key-value store, using the cmap engine
+	if (kv->open("cmap", std::move(cfg)) != status::OK) {
+		cerr << db::errormsg() << endl;
+		exit(1);
+	}
 
-    // create a key-value store using the "cmap" engine
-    db *kv = new db();
-    assert(kv != nullptr);
+	// add some keys and values
+	if (kv->put("key1", "value1") != status::OK) {
+		cerr << db::errormsg() << endl;
+		exit(1);
+	}
+	if (kv->put("key2", "value2") != status::OK) {
+		cerr << db::errormsg() << endl;
+		exit(1);
+	}
+	if (kv->put("key3", "value3") != status::OK) {
+		cerr << db::errormsg() << endl;
+		exit(1);
+	}
 
-    if (kv->open("cmap", std::move(cfg)) != status::OK) {
-        cerr << db::errormsg() << endl;
-        return 1;
-    }
+	// iterate through the key-value store, printing them
+	kv->get_all(kvprint);
 
-    // add some keys and values
-    if (kv->put("key1", "value1") != status::OK) {
-        cerr << db::errormsg() << endl;
-        return 1;
-    }
-    if (kv->put("key2", "value2") != status::OK) {
-        cerr << db::errormsg() << endl;
-        return 1;
-    }
-    if (kv->put("key3", "value3") != status::OK) {
-        cerr << db::errormsg() << endl;
-        return 1;
-    }
+	// stop the pmemkv engine
+	delete kv;
 
-    // iterate through the key-value store
-    kv->get_all(kvprint);
-
-    // stop the engine
-    delete kv;
-
-    return 0;
+	exit(0);
 }
