8c711ac28df4c4d301090becb6dc6f9837621ed1
Steve Scargall <steve.scargall@intel.com>
2019-09-17 11:58:23 -0600

    Added examples for Chapter 7 and 10

---
+++ chapter10/pmem_allocator.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2019 Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * pmem_allocator.cpp - Demonstrates using the pmem::allocator 
+ *                 with std:vector.
+ */
+
+#include <pmem_allocator.h>
+#include <vector>
+#include <cassert>
+ 
+int main(int argc, char *argv[]) {
+	const size_t pmem_max_size = 64*1024*1024; // 64 MB
+	const std::string pmem_dir("/pmemfs/");
+	
+	// Create allocator object
+	pmem::allocator<int> alc(pmem_dir, pmem_max_size);
+	// Create std::vector with our allocator.
+	std::vector<int, pmem::allocator<int> > v(alc);
+	
+	for(int i = 0; i < 100; ++i)
+    	v.push_back(i);
+	
+	for(int i = 0; i < 100; ++i)
+    	assert(v[i] == i);
+	
+	return 0;
+}
---
+++ chapter10/pmem_config.c
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2019 Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY LOG OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* 
+ * pmem_config.c - Demonstrates the use of several configuration 
+ *		   functions within libmemkind.
+ */ 
+
+#include <memkind.h>
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PMEM_MAX_SIZE (1024 * 1024 * 32)
+
+static char path[PATH_MAX] = "pmemfs//";
+
+static void print_err_message(int err)
+{
+    char error_message[MEMKIND_ERROR_MESSAGE_SIZE];
+    memkind_error_message(err, error_message, MEMKIND_ERROR_MESSAGE_SIZE);
+    fprintf(stderr, "%s\n", error_message);
+}
+
+int main(int argc, char *argv[])
+{
+    struct memkind *pmem_kind = NULL;
+    int err = 0;
+
+    if (argc > 2) {
+        fprintf(stderr, "Usage: %s [pmem_kind_dir_path]\n", argv[0]);
+        return 1;
+    } else if (argc == 2 && (realpath(argv[1], path) == NULL)) {
+        fprintf(stderr, "Incorrect pmem_kind_dir_path %s\n", argv[1]);
+        return 1;
+    }
+
+    fprintf(stdout,
+            "This example shows how to use custom configuration to create pmem kind."
+            "\nPMEM kind directory: %s\n", path);
+
+    struct memkind_config *test_cfg = memkind_config_new();
+    if (!test_cfg) {
+        fprintf(stderr, "Unable to create memkind cfg.\n");
+        return 1;
+    }
+
+    memkind_config_set_path(test_cfg, path);
+    memkind_config_set_size(test_cfg, PMEM_MAX_SIZE);
+    memkind_config_set_memory_usage_policy(test_cfg,
+                                           MEMKIND_MEM_USAGE_POLICY_CONSERVATIVE);
+
+
+    // Create PMEM partition with specific configuration
+    err =  memkind_create_pmem_with_config(test_cfg, &pmem_kind);
+    if (err) {
+        print_err_message(err);
+        return 1;
+    }
+
+    err = memkind_destroy_kind(pmem_kind);
+    if (err) {
+        print_err_message(err);
+        return 1;
+    }
+
+    memkind_config_delete(test_cfg);
+
+    fprintf(stdout,
+            "PMEM kind and configuration was successfully created and destroyed.\n");
+
+    return 0;
+}
